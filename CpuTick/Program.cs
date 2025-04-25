d675ad60edcbf2a3a24e8a874f3c78492dee3c5f
Mehdi Babalui <76075816+mahdimrm@users.noreply.github.com>
2025-04-25 16:30:36 +0330

    update : make some improvments to code

--- CpuTick/Program.cs
+++ CpuTick/Program.cs
@@ -6,55 +6,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("This is a simple console application that allows you to know how long it takes a CPU to perform tasks based on its capabilities.");
+        Console.WriteLine("This is a simple console application that benchmarks CPU performance by simulating task execution.");
 
-        bool isRunning = true;
-        int currentStep = 1;
+        var cpuTools = new CpuTools();
+        var cpuInfo = cpuTools.GetCpuInfo();
 
-        CpuTools cpuTools = new CpuTools();
+        int tasksCount = PromptForTaskCount(cpuInfo);
 
-        CpuModel cpuInfo = cpuTools.GetCpuInfo();
-
-        int tasksCount = 0;
-
-        while (isRunning)
-        {
-            switch (currentStep)
-            {
-                case 1:
-                    Console.WriteLine("Please Enter Tasks Count");
-
-                    tasksCount = int.Parse(Console.ReadLine());
-                    if (tasksCount <= 0)
-                    {
-                        Console.WriteLine("Invalid task count. Calculating a recommended value...");
-                        int cyclesPerTask = 1;
-                        tasksCount = cpuInfo.Cores * cpuInfo.MaxClockSpeed * 60 / cyclesPerTask;
-                        Console.WriteLine($"Estimated Task Count Based on CPU: {tasksCount}");
-                    }
-
-                    currentStep++;
-                    break;
-
-                case 2:
-                    var stopWatch = new Stopwatch();
-                    HeavyTask heavyTask = new HeavyTask();
-
-                    stopWatch.Start();
-                    heavyTask.Run(tasksCount);
-                    stopWatch.Stop();
-
-                    isRunning = false;
-                    Console.WriteLine($"Tasks Completed: {tasksCount} | Elapsed Time: {stopWatch.Elapsed.TotalMilliseconds:N0} ms");
-                    break;
-
-                default:
-                    Console.WriteLine("Unknow Step");
-                    isRunning = false;
-                    break;
-            }
-        }
+        RunBenchmark(tasksCount);
 
         Console.ReadKey();
+    }
+
+    static int PromptForTaskCount(CpuModel cpuInfo)
+    {
+        Console.WriteLine("Please enter the number of tasks:");
+
+        if (!int.TryParse(Console.ReadLine(), out int tasksCount) || tasksCount <= 0)
+        {
+            Console.WriteLine("Invalid input. Calculating a recommended task count based on your CPU...");
+            int cyclesPerTask = 1;
+            tasksCount = cpuInfo.Cores * cpuInfo.MaxClockSpeed * 60 / cyclesPerTask;
+            Console.WriteLine($"Recommended Task Count: {tasksCount}");
+        }
+
+        return tasksCount;
+    }
+
+    static void RunBenchmark(int tasksCount)
+    {
+        var stopwatch = new Stopwatch();
+        var heavyTask = new HeavyTask();
+
+        stopwatch.Start();
+        heavyTask.Run(tasksCount);
+        stopwatch.Stop();
+
+        Console.WriteLine($"Tasks Completed: {tasksCount} | Elapsed Time: {stopwatch.Elapsed.TotalMilliseconds:N0} ms");
     }
 }
